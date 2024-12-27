9852a1787659c8eae2ff4cdf94fdafe69652f907
Thalia La Pommeray <141669637+thalialapom@users.noreply.github.com>
2024-12-27 11:16:43 -0500

    init

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Thalia La Pommeray on 12/26/24.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
